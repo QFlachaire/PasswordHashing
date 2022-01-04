03907b0a121a10db1bce9a66baa5c59f41889867
Quentin Flachaire <flachaire.quentin@gmail.com>
2022-01-04 19:34:52 +0100

    Add wrong command handling.
     Changes to be committed:
    	modified:   main.py

--- main.py
+++ main.py
@@ -9,19 +9,27 @@ keyset_handle = new_keyset_handle(
 )
 daead_primitive = keyset_handle.primitive(daead.DeterministicAead)
 
-if __name__ == "__main__":
-    while True:
-        command = input("Register (R) or Login (L)?\n")
-        usr = input("Insert username:\n")
-        pwd = input("Insert password:\n")
 
-        if command == "R":
-            if save_to_database(usr, pwd):
-                print("Register successful\n")
-            else: 
-                print("Wrong Regsitration\n")
-        elif command == "L":
-            if check_password(usr, pwd):
-                print("Login successful\n")
-            else: 
-                print("Wrong Username/Password\n")
+file = open("pwds.txt", "w")
+file.close()
+file = open("users.txt", "w")
+file.close()
+
+
+while True:
+    command = input("Register (R) or Login (L)?\n")
+    if command not in ["R", "L"]:
+        continue
+    usr = input("Insert username:\n")
+    pwd = input("Insert password:\n")
+
+    if command == "R":
+        if save_to_database(usr, pwd):
+            print("Register successful\n")
+        else: 
+            print("Wrong Regsitration\n")
+    elif command == "L":
+        if check_password(usr, pwd):
+            print("Login successful\n")
+        else: 
+            print("Wrong Username/Password\n")
